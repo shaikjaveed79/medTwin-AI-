954a2c9eeb162d2028944f4c7406c5c000959cc1
Shaik Abdul Rasool <rasoolarshad098@gmail.com>
2026-04-23 07:02:23 +0530

    refactor: expanded globals configuration for clarity

--- eslint.config.js
+++ eslint.config.js
@@ -17,7 +17,9 @@ export default tseslint.config(
 ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+  ...globals.browser,
+},
     },
     plugins: {
       "react-hooks": reactHooks,
